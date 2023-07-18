15e6e1401a75c566d2febd453e1a4eedb2d77b0c
Nafees Hassan <nafeeshassan16@gmail.com>
2023-07-18 17:08:18 +0500

    -fPIC

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ $# = 0 ]; then
 	echo "building release config";
-	g++ -shared src/*.h src/*.cpp -s -O3 -o pluto.so -std=c++11;
+	g++ -shared src/*.h src/*.cpp -s -O3 -fPIC -o pluto.so -std=c++11;
 elif [ $1 = "debug" ]; then
 	echo "building debug config";
-	g++ -shared -DDEBUG src/*.h src/*.cpp -g -o pluto.so -std=c++11;
+	g++ -shared -DDEBUG src/*.h src/*.cpp -fPIC -g -o pluto.so -std=c++11;
 fi
